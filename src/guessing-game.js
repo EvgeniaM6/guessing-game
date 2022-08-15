b9942f8c6494989cebdb268f4ffac92d1043fd7a
EvgeniaM6 <evgminukova6@gmail.com>
2022-08-15 22:40:37 +0300

    feat: create class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
     }
-
+    
+    setRange(min, max) {
+        this.left = min
+        this.right = max
+    }
+    
     guess() {
-
+        this.candidate = Math.round((this.right + this.left) / 2)
+        return this.candidate
     }
 
     lower() {
-
+        this.right = this.candidate
     }
 
     greater() {
-
+        this.left = this.candidate
     }
 }
 
